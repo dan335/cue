ea5c88892d55cdc5228fee3edcf326dd20a2ba1b
dan <danphi@gmail.com>
2015-03-08 01:13:05 -0800

    template fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'danimal:cue',
-    version: '1.0.9',
+    version: '1.0.10',
     summary: 'Job Queue for Meteor',
     git: 'https://github.com/dan335/cue',
     documentation: 'README.md'
