477f0d2954ab114121368c00ff73659c63785dd8
dan <danphi@gmail.com>
2015-09-05 20:25:45 -0700

    versions

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
     name: 'danimal:cue',
-    version: '1.1.3',
+    version: '1.1.5',
     summary: 'Job Queue for Meteor',
     git: 'https://github.com/dan335/cue',
     documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.0.3.1');
-    api.use('mongo');
+    api.versionsFrom('1.1');
+    api.use(['check', 'mongo']);
     api.use('templating', 'client');
     api.use('reactive-var', 'client');
-    api.use('momentjs:moment@2.10.3');
+    api.use('momentjs:moment@2.10.6');
     api.use('fongandrew:find-and-modify@0.2.1');
     api.addFiles([
         'collections.js',
